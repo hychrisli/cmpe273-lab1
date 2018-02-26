7fb266a1e026af3586b290c9b3d7a131c6f8cb82
Chris Li <hychrisli@users.noreply.github.com>
2018-02-26 13:11:22 -0800

    Freelancer client setup

--- calculator/server/routes/calc.js
+++ calculator/server/routes/calc.js
@@ -4,13 +4,13 @@ var router = express.Router();
 router.post('/', function (req, res) {
   const expr = req.body.expr;
   if ( expr === undefined )
-    res.send("Empty Expression");
+    res.status(404).send({"err":"Empty Expression"});
   else{
     try{
       let result = eval(expr);
       res.send(result.toString());
     } catch (SyntaxError) {
-      res.send("Expression Syntax Error");
+      res.status(404).send({"err" : "Expression Syntax Error"});
     }
   }
 });
