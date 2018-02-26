f21163c5f429bdc1afd2afefb8ba7ec8674b9c1d
Chris Li <hychrisli@users.noreply.github.com>
2018-02-26 15:40:50 -0800

    basic dashboard and login page

--- freelancer/client/src/App.js
+++ freelancer/client/src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
+
+import authClient from './components/auth-client'
+import {ProjList} from './components/dashboard/projects';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <Admin authClient={authClient}
+            restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+       <Resource name="projects" list={ProjList} />
+     </Admin>
     );
   }
 }
